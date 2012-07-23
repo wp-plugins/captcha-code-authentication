d6000a1678e097669a1defcbd111cba3bf5e54e1
vinoj.cardoza <vinoj.cardoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-23 10:21:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/captcha-code-authentication/tags/1.1@575950 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ general_options.php
@@ -0,0 +1,132 @@
+<?php
+/* Function to configure Captcha for Wordpress */
+function wp_captcha_general_options(){
+?>
+	<div class="wrap" style="font-family: Verdana;">
+	<table>
+		<tr>
+			<td width="50"><img src="<?php echo WP_CAPTCHA_DIR_URL . 'public/images/form_captcha.gif';?>" /></td>
+			<td><h2><?php _e('Captcha Code Authentication for Wordpress - Options', 'wpcaptchadomain');?></h2></td>
+		</tr>
+	</table>
+	<br /><br />
+<?php
+if(isset($_POST['save_captcha_options'])){
+?>
+    <div id="message" class="updated fade"><p><strong><?php _e('Options saved.', 'wpcaptchadomain'); ?></strong></p></div>
+<?php
+	if(isset($_POST['captcha_login'])) 
+		update_option('wpcaptcha_login', $_POST['captcha_login']);
+	if(isset($_POST['captcha_register'])) 
+		update_option('wpcaptcha_register', $_POST['captcha_register']);
+	if(isset($_POST['captcha_lost'])) 
+		update_option('wpcaptcha_lost', $_POST['captcha_lost']);
+	if(isset($_POST['captcha_comments'])) 
+		update_option('wpcaptcha_comments', $_POST['captcha_comments']);
+	if(isset($_POST['captcha_registered'])) 
+		update_option('wpcaptcha_registered', $_POST['captcha_registered']);
+	if(isset($_POST['captcha_type'])) 
+		update_option('wpcaptcha_type', $_POST['captcha_type']);
+	if(isset($_POST['captcha_letters'])) 
+		update_option('wpcaptcha_letters', $_POST['captcha_letters']);
+}
+
+	$c_login = get_option('wpcaptcha_login');
+	if($c_login == 'yes') $c_login_yes = 'selected="selected"';
+	else $c_login_no = 'selected="selected"';
+	$c_register = get_option('wpcaptcha_register');
+	if($c_register == 'yes') $c_register_yes = 'selected="selected"';
+	else $c_register_no = 'selected="selected"';
+	$c_lost = get_option('wpcaptcha_lost');
+	if($c_lost == 'yes') $c_lost_yes = 'selected="selected"';
+	else $c_lost_no = 'selected="selected"';
+	$c_comments = get_option('wpcaptcha_comments');
+	if($c_comments == 'yes') $c_comments_yes = 'selected="selected"';
+	else $c_comments_no = 'selected="selected"';
+	$c_registered = get_option('wpcaptcha_registered');
+	if($c_registered == 'yes') $c_registered_yes = 'selected="selected"';
+	else $c_registered_no = 'selected="selected"';
+	$c_type = get_option('wpcaptcha_type');
+	$c_letters = get_option('wpcaptcha_letters');
+?>
+	<form method="post" action="">
+	<table>
+		<tr height="40">
+			<td><b><?php print __("Enable Captcha for Login form");?>: </b></td>
+			<td>
+				<select name="captcha_login" style="width:75px;margin:0;">
+					<option value="yes" <?php echo $c_login_yes;?>>Yes</option>
+					<option value="no" <?php echo $c_login_no;?>>No</option>
+				</select>			
+			</td>
+		</tr>
+		<tr height="40">
+			<td><b><?php print __('Enable Captcha for Register form');?>: </b></td>
+			<td>
+				<select name="captcha_register" style="width:75px;margin:0;">
+					<option value="yes" <?php echo $c_register_yes;?>>Yes</option>
+					<option value="no" <?php echo $c_register_no;?>>No</option>
+				</select>			
+			</td>
+		</tr>
+		<tr height="40">
+			<td><b><?php print __('Enable Captcha for Lost Password form');?>: </b></td>
+			<td>
+				<select name="captcha_lost" style="width:75px;margin:0;">
+					<option value="yes" <?php echo $c_lost_yes;?>>Yes</option>
+					<option value="no" <?php echo $c_lost_no;?>>No</option>
+				</select>			
+			</td>
+		</tr>
+		<tr height="40">
+			<td><b><?php print __('Enable Captcha for Comments form');?>: </b></td>
+			<td>
+				<select name="captcha_comments" style="width:75px;margin:0;">
+					<option value="yes" <?php echo $c_comments_yes;?>>Yes</option>
+					<option value="no" <?php echo $c_comments_no;?>>No</option>
+				</select>			
+			</td>
+		</tr>
+		<tr height="40">
+			<td><b><?php print __('Hide Captcha for logged in users');?>: </b></td>
+			<td>
+				<select name="captcha_registered" style="width:75px;margin:0;">
+					<option value="yes" <?php echo $c_registered_yes;?>>Yes</option>
+					<option value="no" <?php echo $c_registered_no;?>>No</option>
+				</select>			
+			</td>
+		</tr>
+	</table>
+	<br />
+	<h3><?php print __('Captcha Options');?></h3>
+	<table>
+		<tr height="40">
+			<td><b><?php print __('Select Captcha letters type');?>: </b></td>
+			<td>
+				<select name="captcha_letters" style="width:175px;margin:0;">
+					<option value="capital" <?php if($c_letters == 'capital') echo 'selected="selected"';?>><?php _e('Capital letters only');?></option>
+					<option value="small" <?php if($c_letters == 'small') echo 'selected="selected"';?>><?php _e('Small letters only');?></option>
+					<option value="capitalsmall" <?php if($c_letters == 'capitalsmall') echo 'selected="selected"';?>><?php _e('Capital & Small letters');?></option>
+				</select>			
+			</td>
+		</tr>
+		<tr height="40">
+			<td><b><?php print __('Select a Captcha type');?>: </b></td>
+			<td>
+				<select name="captcha_type" style="width:175px;margin:0;">
+					<option value="alphanumeric" <?php if($c_type == 'alphanumeric') echo 'selected="selected"';?>><?php _e('Alphanumeric');?></option>
+					<option value="alphabets" <?php if($c_type == 'alphabets') echo 'selected="selected"';?>><?php _e('Alphabets only');?></option>
+					<option value="numbers" <?php if($c_type == 'numbers') echo 'selected="selected"';?>><?php _e('Numbers only');?></option>
+				</select>			
+			</td>
+		</tr>
+		<tr height="60">
+			<td><input type="submit" name="save_captcha_options" value="Save options" /></td>
+			<td></td>
+		</tr>
+	</table>
+	</form>
+	</div>
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Captcha ===
+Contributors: vinoj.cardoza
+Donate link: http://www.fingerfish.com/
+Tags: captcha, wordpress, wordpress captcha, captcha for wordpress, form captcha, forms captcha, captcha security, security
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: trunk
+License: GPLv2 or later
+
+Adds Captcha Code anti-spam methods to WordPress forms. Forms include comments form, registration form, lost passwordform and login form. In order to post comments or register, users will have to type in the code shown on the image. This prevents spam from automated bots. Adds security.
+
+== Description ==
+Adds Captcha Code anti-spam methods to WordPress forms. Forms include comments form, registration form, lost passwordform and login form. In order to post comments or register, users will have to type in the code shown on the image. This prevents spam from automated bots. Adds security.
+
+= Features =
+1. Administrator can specify where the captcha should be displayed i.e, comments, login, registration or lost password form.
+2. Administrator select the letters type from the options available - Capital letters, Small letters or Captial & Small letters.
+3. Administrator select the captcha type from the options available - Alphanumeric, Alphabets or numbers.
+
+= Note =
+1. If you find any bugs, please report in the following link, so that it will be fixed as quick as possible.
+2. If you think any feature adding to this plugin can improve its features, please recommend it in the following link.
+
+= Donations =
+
+Thanks for downloading and installing my plugin. You can show your appreciation and support future development by donating. To donate visit my site http://www.fingerfish.com.
+
+For more visit: http://fingerfish.com/captcha-code-authentication/
+
+== Installation ==
+
+1. Download the plugin.
+2. Upload to your blog (/wp-content/plugins/).
+3. Activate it.
+4. Click the 'Captcha' menu.
+6. Fill in the options.
+
+Important Note: It is mandatory to save options in this plugin.
+
+You're done!
+
+Uninstalling is as simple as deactivating the plugin.
+
+== Screenshots ==
+
+1. screenshot-1.png
+2. screenshot-2.png
+3. screenshot-3.png
+4. screenshot-4.png
+
+== Change Log ==
+
+= Version 1.1 =
+* New feature added to select the captcha letter type.
+* New feature added to select the captcha type.
